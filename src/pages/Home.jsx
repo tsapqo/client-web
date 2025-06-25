58c37c69db48d5d13d5cfdf5c81b99e375935c6a
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-06-25 22:08:58 +0300

    finxing deployment problem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import Hero from "../components/sections/home/Hero";
 import LeaderboardSection from "../components/sections/home/Leaderboard";
-import LessonsSection from "../components/sections/home/Lessons";
+// import LessonsSection from "../components/sections/home/Lessons";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <LeaderboardSection />
-      <LessonsSection />
+      {/* <LessonsSection /> */}
     </>
   );
 };
